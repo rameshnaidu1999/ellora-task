5d7d3ca511cf91e9e54d218fa37b8e7dcc473e6a
rameshnaidu1999 <rameshnaidu1999@gmail.com>
2020-09-17 19:41:36 +0530

    made it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Clients from './components/Clients'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import Services from './components/Services'
+import Works from './components/Works'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Home />
+      <Services />
+      <Works />
+      <Clients />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
